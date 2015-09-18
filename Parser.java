c6c69aa4d2f81c60513d0c057074f368fc94abb2
rolandkofler <roland.kofler@gmail.com>
2015-09-18 21:50:58 +0200

    Update Parser.java
    Didn't want to touch alot the exposed interface but this getter setter of the file really disturbed in executing my plan to make it threadsafe. 
    File is always a leak, but at least we should not encourage it by contract.
    changes are also commented inline. Here the succus:
    (1) imutable file, (2) internal DRY method for content reading, then (3) locking it was more efficient. Could have used a sync(lock){} mutex logic but preferred the more semantically explicit read and write lock logic of ReentrantReadWriteLock.
    (4) not clear why its called getContentWithoutUnicode was, because a quick check shows that you check for Unicode Block Basic Latin

--- Parser.java
+++ Parser.java
@@ -1,42 +1,62 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	private final File file; // keep it immutable
+	private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+	public Parser(File f) {
+		file = f;
+	}
+
+	// you wanted to be threadsafe
+	public String getContent() {
+
+	}
+
+	// you wanted to be threadsafe
+	public String getContentWithoutUnicode() {
+		return getContent(false);
+	}
+
+	// dont repeat yourself
+	private String getContent(boolean withUnicode) throws IOException {
+		lock.readLock().lock(); // lock the whole file access
+		String output = "";
+		try {
+			FileInputStream i = new FileInputStream(file); // bufferedstream  recommended
+			int data;
+
+			while ((data = i.read()) > 0) {
+				if (withUnicode || data < 0x80) { // is this what you intent with non unicode? hmm
+					output += (char) data;
+				}
+			}
+		} finally {
+			lock.readLock().unlock(); // Unlock read
+		}
+
+		return output;
+	}
+
+	// thread safe
+	public void saveContent(String content) throws IOException {
+
+		lock.writeLock().lock();
+		try {
+			FileOutputStream o = new FileOutputStream(file); // bufferedstream  recommended
+			for (int i = 0; i < content.length(); i += 1) {
+				o.write(content.charAt(i));
+			}
+		} finally {
+			lock.readLock().unlock(); // Unlock read
+		}
+	}
 }
